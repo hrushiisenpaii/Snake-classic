eac920623bd2b89bfdc6ab2401d7f691bd5dff15
Hrushikesh Patra <hrushikesh.patra24@gmail.com>
2022-08-14 19:43:43 +0530

    changes so screen wont move

--- snake.js
+++ snake.js
@@ -65,16 +65,16 @@ function createRect(x,y,width, height,color) {
 
 window.addEventListener("keydown", (event) => {
     setTimeout(() => {
-        if (event.keyCode == 65 && snake.rotateX != 1) {
+        if (event.keyCode == 65 || event.keyCode == 37 && snake.rotateX != 1) { //left
             snake.rotateX = -1
             snake.rotateY = 0
-        } else if (event.keyCode == 87 && snake.rotateY != 1) {
+        } else if (event.keyCode == 87 || event.keyCode == 38 && snake.rotateY != 1) { //up
             snake.rotateX = 0
             snake.rotateY = -1
-        } else if (event.keyCode == 68 && snake.rotateX != -1) {
+        } else if (event.keyCode == 68 || event.keyCode == 39 && snake.rotateX != -1) { //right
             snake.rotateX = 1
             snake.rotateY = 0
-        } else if (event.keyCode == 83 && snake.rotateY != -1) {
+        } else if (event.keyCode == 83 || event.keyCode == 40 && snake.rotateY != -1) { //down
             snake.rotateX = 0
             snake.rotateY = 1
         }
@@ -148,6 +148,12 @@ class Apple{
         }
     }
 }
+
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
 
 const snake = new Snake(20,20,20);
 let apple = new Apple();
